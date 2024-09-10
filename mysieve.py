d49463382a77fdcbfdb060874fed391e415c5b9e
SERRAJOthman <iamothmanserraj@gmail.com>
2024-09-10 16:24:58 +0100

    Update mysieve.py

--- mysieve.py
+++ mysieve.py
@@ -1,29 +1,35 @@
-premiers=[2,3,5,7]
-sortie=premiers
-facteurs=[1,2]
-P=2
-restes=[1]
-c=1
-for init in range(4):
-	F=sortie[init+1]
-	c+=1
-	for o in range(facteurs[-1]+1,F):
-		facteurs.append(o)
-	H=[]
-	for f in facteurs:
-		for r in restes:
-			H.append(P*f+r)
-	if init<1:P*=H[0]
-	else:P*=restes[2]
-	X=[]
-	u=0
-	for x in H[2:]:
-		for ci in range(1,c):
-			if x%sortie[init+ci]==0:
-				u=0
-				break
-			else:u=1
-		if u==1:X.append(x)
-	restes=[1]+H[:2]+X
-	sortie+=X
-	print("sortie:",sortie)
+def Liste_facteures(old_P,old_facteures,pold,pnext):
+	facteures=old_facteures
+	P=old_P*pold
+	for f in range(pold,pnext):
+		facteures.append(f)
+	return facteures,P
+def Liste_restes(old_restes,old_P,pold,pnext,old_facteures):
+	facteures,P=Liste_facteures(old_P,old_facteures,pold,pnext)
+	restes=[1]
+	for f in facteures:
+		for r in old_restes:
+			restes.append(P*f+r)
+	return restes,P,pnext,restes[1],facteures
+def sievage(Hilk,x):
+	g=Hilk[x]
+	Hulk=Hilk[:x+1]
+	for m in Hilk[x:]:
+		if m%g!=0:
+			Hulk.append(m)
+	return Hulk,g
+old_restes,old_P,pold,pnext,old_facteures=Liste_restes([1,5],2,3,5,[1,2])
+print([1,5],2,3,5,[1,2])
+Hilk=[2,3,5]
+print(Hilk)
+x=1
+for k in range(2):
+	x+=1
+	Hilk+=old_restes[1:]
+	print(Hilk,sievage(Hilk,x))
+	print(old_restes,old_P,pold,pnext,old_facteures)
+	old_restes,old_P,pold,pnext,old_facteures=Liste_restes(old_restes,old_P,pold,pnext,old_facteures)
+print(old_restes,old_P,pold,pnext,old_facteures)
+x+=1
+Hilk+=old_restes[1:]
+print(Hilk,sievage(Hilk,x))
