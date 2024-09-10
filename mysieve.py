63ad2504fb1cd4fa3110846c48a13d2f1e7a13cd
SERRAJOthman <iamothmanserraj@gmail.com>
2024-09-10 05:30:53 +0100

    Update mysieve.py

--- mysieve.py
+++ mysieve.py
@@ -1,17 +1,23 @@
-premiers=[2]
+premiers=[2,3,5,7]
 sortie=premiers
 facteurs=[1,2]
 P=2
 restes=[1]
 for init in range(3):
-	F=premiers[-1]
+	F=premiers[init+1]
 	for o in range(facteurs[-1]+1,F):
 		facteurs.append(o)
 	H=[]
 	for f in facteurs:
 		for r in restes:
 			H.append(P*f+r)
-	P*=H[0]
-	restes=[1]+H
-	sortie+=H
+	if init<1:P*=H[0]
+	else:P*=restes[2]
+	X=[]
+	for x in H[2:]:
+		if x%F==0:
+			continue
+		X.append(x)
+	restes=[1]+H[:2]+X
+	sortie+=X
 	print("sortie:",sortie)
