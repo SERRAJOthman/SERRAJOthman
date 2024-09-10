8caa8c3b3be84b7a47debeb4c1f3c2daded612c1
SERRAJOthman <iamothmanserraj@gmail.com>
2024-09-10 01:43:01 +0100

    Update mysieve.py

--- mysieve.py
+++ mysieve.py
@@ -1,64 +1,31 @@
-#premiers=[2]
-#facteurs=[1]
-#restes=[1,5]
-#1<reste(n)<produit_premiers(n)-1
-#1<facteur(n)<premier(n+1)-1
-#formule_produit_premier=
-#produit_premiers(i)*facteur(j)+reste(k)
-def Lrestes(premiers):
-	P=1
-	restes=[]
-	for premier in premiers:
-		P*=premier
-	for o in range(1,P):
-		restes.append(o)
-		for premier in premiers:
-			if restes[-1]%premier==0: 
-				restes.pop()
-	return restes,P
-def Lfacteurs(premiers):
-	F=premiers[-1]
+premiers=[2,3,5,7]
+sortie=premiers
+number=2
+for init in range(number):
+	print("premiers:",premiers)
+	#Calcul des facteurs
+	n=len(premiers)-2
+	F=premiers[n+1]
 	facteurs=[]
-	for t in range(1,F+1):
-		facteurs.append(t)
-	return facteurs
-#restes,P=Lrestes(premiers)
-#facteurs=Lfacteurs(premiers)
-#H=P,facteurs,restes
-#print(H)
-def formule_produit_premier(H):
-	premiers_potentiels=[]
-	for f in facteurs:
-		for r in restes:
-			h=P*f+r
-			premiers_potentiels.append(h)
-	return premiers_potentiels
-#premiers_bis=premiers+restes[1:]+formule_produit_premier(H)
-#print(premiers_bis)
-
-def formule_generale(n):
-	premiers=[2,3]
-	premiers_potentiels=[7,11,13,17,19,23]
-	for l in range(n):
-		P=1
-		facteurs=[]
-		restes=[]
+	for o in range(1,F):
+		facteurs.append(o)
+	print("facteurs:",facteurs)
+	#Calcul des restes
+	restes=[]
+	P=1
+	for p in premiers[:n+1]:P*=p#mettre au debut
+	for t in range(1,P):
+		restes.append(t)
 		for premier in premiers:
-			P*=premier
-		for o in range(1,P):
-			restes.append(o)
-			for premier in premiers:
-				if restes[-1]%premier==0: 
-					restes.pop()
-		F=premiers[-1]
-		for t in range(1,F+1):
-			facteurs.append(t)
-	premiers_potentiels=[]
+			if restes[-1]%premier==0:
+				restes.pop()
+	print("restes:",restes)
+	sortie+=restes[1:]
+	print("sortie:",sortie)
+	#Calcul des h
+	H=[]
 	for f in facteurs:
 		for r in restes:
-			h=P*f+r
-			premiers_potentiels.append(h)
-	premiers_bis=premiers+restes[1:]+premiers_potentiels
+			H.append(P*f+r)
 	premiers.append(restes[1])
-	return premiers,restes,facteurs,premiers_potentiels	
-print(formule_generale(2))
+	print("H:",H,"premiers:",premiers)
