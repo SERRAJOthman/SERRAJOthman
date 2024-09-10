f372974e0e2d71989ff1f067eea2e00a913b1d27
SERRAJOthman <iamothmanserraj@gmail.com>
2024-09-10 03:47:10 +0100

    Update mysieve.py

--- mysieve.py
+++ mysieve.py
@@ -1,31 +1,33 @@
-premiers=[2,3,5,7]
+premiers=[2,3,5]
 sortie=premiers
-number=2
-for init in range(number):
+facteurs=[1,2]
+P=6
+restes=[1,5]
+print("restes:",restes)
+for init in range(2):
 	print("premiers:",premiers)
 	#Calcul des facteurs
-	n=len(premiers)-2
-	F=premiers[n+1]
-	facteurs=[]
-	for o in range(1,F):
+	F=premiers[-1]
+	for o in range(facteurs[-1]+1,F):
 		facteurs.append(o)
 	print("facteurs:",facteurs)
-	#Calcul des restes
-	restes=[]
-	P=1
-	for p in premiers[:n+1]:P*=p#mettre au debut
-	for t in range(1,P):
-		restes.append(t)
-		for premier in premiers:
-			if restes[-1]%premier==0:
-				restes.pop()
-	print("restes:",restes)
-	sortie+=restes[1:]
-	print("sortie:",sortie)
-	#Calcul des h
+	#Calcul des h futur r
 	H=[]
 	for f in facteurs:
 		for r in restes:
+			print(P,f,r)
 			H.append(P*f+r)
-	premiers.append(restes[1])
-	print("H:",H,"premiers:",premiers)
+	h0=H[0]
+	P*=restes[1]
+	premiers.append(h0)
+	print("P:",P)
+	print("H:",H)
+	print("premiers:",premiers)
+	X=[]
+	for x in H:
+		if x%premiers[-2]!=0 or x==premiers[-2]:
+			X.append(x)
+	restes=[1]+X
+	print("restes:",restes)
+	sortie+=restes[1:]
+	print("sortie:",sortie)
